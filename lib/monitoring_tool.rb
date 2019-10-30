ac6fd549d1fe60fec6b89e8514e897942f5fb14f
XiaoDao <liu_xiaodao@163.com>
2019-10-30 12:51:54 +0800

    init

---
+++ lib/monitoring_tool.rb
@@ -0,0 +1,6 @@
+require "monitoring_tool/version"
+
+module MonitoringTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/monitoring_tool/version.rb
@@ -0,0 +1,3 @@
+module MonitoringTool
+  VERSION = "0.1.0"
+end
